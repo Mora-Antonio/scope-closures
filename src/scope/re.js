b2b5072a395b9820ec39dcd2e21ef9262f9c79fe
Mora-Antonio <miguel.antonio290120@gmail.com>
2022-07-14 23:43:00 -0600

    Agregamos todos los arhcivos

---
+++ src/scope/re.js
@@ -0,0 +1,15 @@
+var firstname; //undefined
+firstname = 'Miguel';
+
+firstname = 'Antonio';
+var firstname = 'Julio'
+console.log(firstname);
+
+let secondName = 'Antonio';
+secondName = 'Julio';
+// let secondName = 'Jose';
+console.log(secondName);
+
+const country = 'Guatemala';
+country = 'Japon'
+console.log(country);
